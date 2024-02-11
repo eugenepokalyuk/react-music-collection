1503ee10f78a792963e92f6e772e47ef873158a8
Evgenii <eugene.pokalyuk@gmail.com>
2024-02-12 02:28:53 +0700

    Add widget :)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import About from '../About/About';
 import Collection from '../Collection/Collection';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import MarqueeBanner from '../MarqueeBanner/MarqueeBanner';
+import ShapeWidget from '../ShapeWidget/ShapeWidget';
 
 const AnimatedBanner = () => (
   <section className="p-4 flex justify-center items-center">
@@ -12,13 +15,28 @@ const AnimatedBanner = () => (
 
 const App = () => {
   const playlists = [1006, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015, 1017, 1022, 1024, 1026, 1027, 1028];
+
+  const [bannerVisible, setBannerVisible] = useState<boolean>(true);
+  const { ref, inView } = useInView({
+    threshold: 0.1
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setBannerVisible(false)
+    } else {
+      setBannerVisible(true)
+    }
+  }, [inView]);
+
   return (
     <>
       <Header />
       <MarqueeBanner />
       <Collection playlists={playlists} />
       <AnimatedBanner />
-      <About />
+      <ShapeWidget visible={bannerVisible} />
+      <div ref={ref}><About /></div>
       <Footer />
     </>
   )
---
+++ src/components/ShapeWidget/ShapeWidget.tsx
@@ -0,0 +1,29 @@
+import { FC, useState } from "react";
+
+interface ShapeWidgetProps {
+    visible: boolean
+}
+
+const ShapeWidget: FC<ShapeWidgetProps> = ({ visible }) => {
+    const [showMessage, setShowMessage] = useState(false);
+
+    const handleClick = () => {
+        setShowMessage(true);
+        setTimeout(() => {
+            setShowMessage(false);
+        }, 3000);
+    };
+
+    return (
+        <div className={`fixed bottom-10 right-10 md:flex-1 flex justify-center mb-6 md:mb-0 shape-widget ${visible ? 'shape-widget-visible z-10' : 'shape-widget-hidden'}`} onClick={handleClick}>
+            <div className="shape max-sm:w-24 max-sm:h-24 md:w-48 md:h-48 bg-blue-300 rounded-full"></div>
+
+            <div className={`message ${showMessage ? "message-show" : "message-hidden"}`}>
+                <h1>Привет!</h1>
+                <p>Я помощник Евгения, умею только здороваться!</p>
+            </div>
+        </div>
+    );
+};
+
+export default ShapeWidget;
