88aa76e5ece8743660e67333a111f8de4ca92448
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 16:37:59 +0700

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,6 +5,7 @@ const About = () => {
                 <div className="md:flex-1 flex justify-center mb-6 md:mb-0">
                     <div className="shape max-sm:w-60 max-sm:h-60 w-48 h-48 bg-blue-300 rounded-full"></div>
                 </div>
+                
                 <div className="md:flex-1 px-6">
                     <p className="text-gray-600 text-lg">Я очень рад поделиться со всеми вами своей музыкой. Ничто так не объединяет звук и душу, и я дорожу каждым моментом этого.</p>
                 </div>
