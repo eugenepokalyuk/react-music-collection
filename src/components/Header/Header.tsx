68db6dac9464d78a5524b1d5389a66f00fbc4c40
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 16:39:31 +0700

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 const Header = () => (
-    <header className="p-4 text-white text-left lg:text-center"> {/* lg:h-40 устанавливает высоту в 160px (40 * 4px) начиная с больших экранов */}
+    <header className="p-4 text-white text-left lg:text-center">
         <h1 className="text-[40px] lg:text-[160px]">Мой Плейлист</h1>
     </header>
 );
