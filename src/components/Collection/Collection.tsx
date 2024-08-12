386b8cc5febd06766b1103602fabad586d2c0e55
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 16:39:07 +0700

    Update Collection.tsx

--- src/components/Collection/Collection.tsx
+++ src/components/Collection/Collection.tsx
@@ -3,9 +3,11 @@ import { FC } from 'react'
 type CollectionProps = {
     playlists: number[]
 }
+
 type PlaylistCardProps = {
     playlist: number
 }
+
 const PlaylistCard: FC<PlaylistCardProps> = ({ playlist }) => (
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2">
         <div className="rounded-2xl overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
@@ -16,6 +18,7 @@ const PlaylistCard: FC<PlaylistCardProps> = ({ playlist }) => (
                 height="450"
                 src={`https://music.yandex.ru/iframe/playlist/mackspavlov/${playlist}`}
                 title={`Yandex Music Playlist ${playlist}`}>
+                
                 <a href='https://music.yandex.ru/users/mackspavlov'>Евгений Покалюк</a> на Яндекс Музыке
             </iframe>
         </div>
