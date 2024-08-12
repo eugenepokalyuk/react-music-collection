35ff8fa8dcffc7a33a6abe05a5e4ba0ab462a378
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 16:39:50 +0700

    Update MarqueeBanner.tsx

--- src/components/MarqueeBanner/MarqueeBanner.tsx
+++ src/components/MarqueeBanner/MarqueeBanner.tsx
@@ -5,21 +5,26 @@ const MarqueeBanner = () => {
         "НЕПРЕРЫВНОЕ ОБУЧЕНИЕ • РАЗВИТИЕ И РОСТ • ВМЕСТЕ К СВЕТЛОМУ БУДУЩЕМУ • ПРОГРАММИРУЕМ МИР • НОВЫЕ ВОЗМОЖНОСТИ КАЖДЫЙ ДЕНЬ • ОБЩАЕМСЯ С КОДОМ",
         "ВДОХНОВЛЯЕМ ИННОВАЦИЯМИ • ПРЕОДОЛЕВАЕМ ВЫЗОВЫ • СТРОИМ КОД И СООБЩЕСТВО • СКЕТЧИМ, КОДИМ, СОЗДАЁМ • ДЕЛИМСЯ ЗНАНИЯМИ И ОПЫТОМ",
         "ХАКАТОНЫ КАЖДУЮ НЕДЕЛЮ • ПРОЕКТИРУЕМ БУДУЩЕЕ • ЖИВЁМ КРЕАТИВОМ • РАБОТАЕМ ИГРАЯ • МЕЧТАЕМ И РЕАЛИЗУЕМ • НЕТ ГРАНИЦ ДЛЯ ФАНТАЗИИ"
-    ]
+    ];
+    
     return (
         <section>
             <div className="marquee p-2 border-t-[2px] border-b-[2px] border-white/50">
                 <span className="marquee-content text-white/50 text-9xl" data-text={texts[0]}>{texts[0]}</span>
             </div>
+    
             <div className="marquee p-2 border-b-[2px] border-white/50">
                 <span className="marquee-content-reverse text-white/50 text-9xl" data-text={texts[1]}>{texts[1]}</span>
             </div>
+            
             <div className="marquee p-2 border-b-[2px] border-white/50">
                 <span className="marquee-content text-white/50 text-9xl" data-text={texts[2]}>{texts[2]}</span>
             </div>
+            
             <div className="marquee p-2 border-b-[2px] border-white/50">
                 <span className="marquee-content-reverse text-white/50 text-9xl" data-text={texts[3]}>{texts[3]}</span>
             </div>
+            
             <div className="marquee p-2 border-b-[2px] border-white/50">
                 <span className="marquee-content text-white/50 text-9xl" data-text={texts[4]}>{texts[4]}</span>
             </div>
